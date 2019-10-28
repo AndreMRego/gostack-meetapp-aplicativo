e5dbaa192c6fc4b4d1b1b1620040fae63544599c
Andre Rego <andremregodev@gmail.com>
2019-10-28 00:13:45 -0300

    Criando estrutura de pastas, telas de Dashboard, Profile, Login, Cadastro e Subscription

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from 'react'
-import { View } from 'react-native'
+import { PersistGate } from 'redux-persist/integration/react'
+import { Provider } from 'react-redux'
+import { StatusBar } from 'react-native'
 
-export default function src() {
-  return <View></View>
+import './config/ReactotronConfig'
+
+import { store, persistor } from './store'
+import App from './App'
+
+export default function Index() {
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <StatusBar barStyle="light-content" backgroundColor="#22202C" />
+        <App />
+      </PersistGate>
+    </Provider>
+  )
 }
